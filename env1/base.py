1f0dd52154fe6a98bffca50061fb62b178cfab5b
akhil sreenivasan <akhilsreenivasan7025@gmail.com>
2025-11-21 14:16:53 +0530

    basic structure added

--- env1/base.py
+++ env1/base.py
@@ -1,1 +1,16 @@
-kjjbjhvjhvhj=0
+from binance import Client
+
+API_KEY = '3Ky0TSOXuYJrg6touX5gAKwoac8106VhQ1FooTAncsla5RoALjEfr9qGGIzUF56k'
+API_SECRET = 's6b0mo3gO3IIZKdGjo0pGgbPYTTS84EJRxq7hO0Ah81btVP7MyuiiiqwaziTUN3q'
+
+#connect to binance client
+try:
+    client = Client(API_KEY, API_SECRET, testnet=True)
+    if client:
+        print("Connected to Binance Testnet")
+        balance = client.get_asset_balance(asset='USDT')
+        print(balance)
+    else:
+        print("Failed to connect to Binance Testnet")
+except Exception as e:
+    print(f"An error occurred: {e}")
